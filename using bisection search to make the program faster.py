c6cd1442815476994b86f206892f078aedfe2b1d
Marketos Damigos <33204161+Marketos-Damigos@users.noreply.github.com>
2017-12-10 20:59:13 +0200

    Removed semi-columns and fixed spaces.

--- using bisection search to make the program faster.py
+++ using bisection search to make the program faster.py
@@ -1,25 +1,25 @@
-lower = balance/12.00;
-upper = (balance*(1+(annualInterestRate/12.0))**12/12.0);
-answer = (upper+lower)/2;
+lower = balance/12.00
+upper = (balance*(1+(annualInterestRate/12.0))**12/12.0)
+answer = (upper+lower)/2
 
 def year(pay,myBalance) :
     totalPaid = 0;
     for i in range(1,13):
-        totalPaid += pay;
-        myBalance = myBalance - pay;
-        myBalance = myBalance + ((annualInterestRate/12.00) * myBalance);
+        totalPaid += pay
+        myBalance = myBalance - pay
+        myBalance = myBalance + ((annualInterestRate/12.00) * myBalance)
         if myBalance<=0 :
-            break;
-    return myBalance;
+            break
+    return myBalance
     
 while True :
-    z = year(answer,balance);
+    z = year(answer,balance)
     if abs(z-0.01)<0.1:
-       print("Lowest Payment: " + str(answer));
-       break;     
+        print("Lowest Payment: " + str(answer))
+        break 
     elif z<0:
-       upper = answer;
-       answer = round((upper+lower)/2,2);
+        upper = answer
+        answer = round((upper+lower)/2,2)
     else :
-       lower = answer;
-       answer = round((upper+lower)/2,2);
+        lower = answer
+        answer = round((upper+lower)/2,2)
